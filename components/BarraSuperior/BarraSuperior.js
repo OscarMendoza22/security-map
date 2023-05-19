72e2c7f43eb4c32af455980901f8facc82041389
kevin niño <kevin.nino@aviatecnologia.com.co>
2023-05-19 16:04:37 -0500

    New: autocompletar

--- components/BarraSuperior/BarraSuperior.js
+++ components/BarraSuperior/BarraSuperior.js
@@ -1,47 +1,150 @@
-export default function BarraSuperior() {
+import Logo from "../Logo/Logo";
+import React, { FC, useState } from "react";
+import Autocomplete from "react-autocomplete-select";
+
+const data = [
+  { localidad: "Engativa", peligro: "rojo" },
+  { localidad: "Usaquen", peligro: "rojo" },
+  { localidad: "Suba", peligro: "rojo" },
+  { localidad: "Fontibon", peligro: "rojo" },
+  { localidad: "Kennedy", peligro: "rojo" },
+  { localidad: "Bosa", peligro: "rojo" },
+  { localidad: "Tunjelito", peligro: "rojo" },
+  { localidad: "Ciudad Bolivar", peligro: "rojo" },
+  { localidad: "Puente Aranda", peligro: "rojo" },
+  { localidad: "Teusaquillo", peligro: "rojo" },
+  { localidad: "Barrios Unidos", peligro: "rojo" },
+  { localidad: "Chapinero", peligro: "rojo" },
+  { localidad: "Los Martires", peligro: "rojo" },
+  { localidad: "Candelaria", peligro: "rojo" },
+  { localidad: "Santa fe", peligro: "rojo" },
+  { localidad: "San Cristobal", peligro: "rojo" },
+  { localidad: "Sumapaz", peligro: "rojo" },
+  { localidad: "Usme", peligro: "rojo" },
+  { localidad: "Rafael Uribe", peligro: "rojo" },
+  { localidad: "Antonio Nariño", peligro: "rojo" },
+  { localidad: "Antonio Nariño", peligro: "rojo" },
+];
+
+export default function BarraSuperior({ setCordenada, setZoom }) {
+  const [change, setChange] = useState();
+
+  console.log(change)
+
   return (
-    <div className="flex justify-between mx-10 mb-10">
-      <div className="flex items-center flex-col">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width={69}
-          height={69}
-          fill="none"
-        >
-          <path
-            stroke="#2B2A5B"
-            strokeWidth={1.5}
-            d="M34.41 45.928 29.602 34.99s.798 1.142 4.806 1.142c4.01 0 4.336-1.142 4.336-1.142l-4.336 10.938Z"
-          />
-          <path
-            stroke="#2B2A5B"
-            strokeWidth={2}
-            d="m23.393 7.258 22.516.125 15.833 16.01-.125 22.516-16.01 15.833-22.516-.125-15.833-16.01.125-22.516 16.01-15.833Z"
-          />
-          <path
-            stroke="#2B2A5B"
-            strokeWidth={2}
-            d="m33.808 2 23.183 9.03L67 33.807 57.97 56.99 35.193 67 12.01 57.97 2 35.193l9.03-23.183L33.807 2Z"
-          />
-          <path
-            stroke="#2B2A5B"
-            strokeWidth={1.5}
-            d="m19.367 49.652 3.743-6.49a1 1 0 0 1 .867-.5H45.35a1 1 0 0 1 .866.5l2.16 3.745 1.584 2.745a1 1 0 0 1-.866 1.5H20.233a1 1 0 0 1-.866-1.5Z"
-          />
-          <circle
-            cx={34.173}
-            cy={26.991}
-            r={9.045}
-            stroke="#2B2A5B"
-            strokeWidth={1.5}
-          />
-          <circle cx={34.173} cy={26.991} r={2.612} fill="#2B2A5B" />
-        </svg>
+    <div className="flex items-center justify-between mx-10 mb-10">
+      <div className="flex items-center flex-col ml-5">
+        <Logo />
         <span>Security Map</span>
       </div>
-      <div className="flex w-[50%] items-end">
-        <input type="text" className="rounded-xl w-full h-10 " />
-      </div>
+      <AutocompletePrueba setChange={setChange}/>
+      {/* <div className="flex items-center bg-white rounded-2xl h-[3em] w-[40%]">
+        <input
+          type="text"
+          id="busqueda"
+          placeholder="Busca tu localidad o barrio"
+          className="w-full rounded-2xl ml-1 h-10 outline-none pl-2"
+        />
+        <span className="material-symbols-outlined mx-2">search</span>
+      </div> */}
     </div>
   );
 }
+
+const AutocompletePrueba = ({ setChange }) => {
+  const inputRef = React.useRef();
+
+  let arr = [];
+
+  // tripAdvisorDestinations.forEach((destino, i) => {
+  //   arr[i] = {
+  //     localidad: `${destino.destinationName} (${destino.timeZone})`,
+  //     peligro: destino.destinationId,
+  //   };
+  // });
+
+  return (
+    <>
+      <Autocomplete
+        inputRef={inputRef}
+        // defaultInputValue={"Bogotá (BOG), Colombia (CO)"}
+        searchPattern={"containsString"}
+        placeholder={"Buscar"}
+        maxOptionsLimit={5}
+        getItemValue={(item) => {
+          return `${item.localidad}`;
+        }}
+        optionsJSX={(value) =>(<li>{value}</li>)}
+        itemsData={data}
+        
+        onChange={(item) => {
+
+          setChange( data.find((el) => el.localidad == item));
+        }}
+        inputJSX={(props) => (
+          <label className="flex items-center bg-white rounded-2xl h-[3em]">
+            <input
+              {...props}
+              required
+              id="inputEnter"
+              className="inputClass w-full rounded-2xl ml-1 h-10 outline-none pl-2"
+            />
+            <span className="material-symbols-outlined mx-2">search</span>
+          </label>
+        )}
+        globalStyle="
+          .___optionsDiv___ {
+          z-index:5000;
+          margin-top: 1em;
+          border-radius: 6px !important;
+          background: rgba(255, 255, 255, 0.8);
+          border: none;
+          }
+          .___optionsDiv___ div{
+          padding: 0;
+          border-bottom: 0;
+          background: transparent
+          }
+          .___optionsDiv___ div:hover {
+          background-color: white; 
+          font-weight: 700;
+          font-size: 20px;
+          }
+          .___optionsDiv___ li {
+          list-style: none; 
+          padding: 2px 20px;
+          font-size: 15px;
+          color: var(--avia-grey-dark);
+          }
+          .___optionsDiv___ div:nth-child(1)::before {
+           content: '';
+           width: 0;
+           height: 0;
+           border-right: 10px solid transparent;
+           border-top: 10px solid transparent;
+           border-left: 10px solid transparent;
+           border-bottom: 10px solid rgba(255, 255, 255, 0.8);
+           position: absolute;
+           top: -20px;
+           left: 10px;
+          }
+          .___optionsDiv___ div:nth-child(1) {
+           padding-top: 10px;
+           border-top-left-radius: 6px 6px;
+           border-top-right-radius: 6px 6px;
+          }
+          .___optionsDiv___ div:last-child {
+           border-bottom-left-radius: 6px 6px;
+           border-bottom-right-radius: 6px 6px;
+           padding-bottom: 10px;
+          }
+          .autocomplete-active {
+           background-color: rgba(146, 138, 138, 0.8) !important; 
+           font-weight: 700;
+           font-size: 20px; 
+          }
+          "
+      />
+    </>
+  );
+};
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,18 @@
-import BarraSuperior from "../components/BarraSuperior/BarraSuperior";
+import { useState } from "react";
 import Map from "../components/Map/index";
+import BarraSuperior from "@/components/BarraSuperior/BarraSuperior";
+
 
 export default function Home() {
-  const cordenadas = [4.60971, -74.08175];
+  const [cordenada, setCordenada] = useState([4.60971, -74.08175]);
+  const [zoom, setZoom] = useState(11);
 
   return (
     <div className="my-10">
       <div className="mx-5">
-        <BarraSuperior />
+        <BarraSuperior setCordenada={setCordenada} setZoom={setZoom} />
       </div>
-      <Map cordenadas={cordenadas} />
+      <Map cordenadas={cordenada} zoom={zoom}/>
     </div>
   );
 }
